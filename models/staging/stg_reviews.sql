e4f2d7b4c255a4e3160b2d99861049ec0d044adc
Adam <adamluba@gmail.com>
2019-06-10 22:04:50 -0700

    materialize views, listings, listing history, reviews

---
+++ models/staging/stg_reviews.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='table') }}
+
+with source as (
+
+    select * from source_data.reviews
+
+),
+
+renamed as (
+
+    select
+
+        id as reviews_id,
+        review as review_id,
+        listing_id,
+        reviewer_name,
+        comments,
+        date as review_date
+
+    from source
+
+)
+
+select * from renamed
